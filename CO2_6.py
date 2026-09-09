4613cbecbd76edea1962ac59170237b8941ead05
Agney Unnithan <agneyunnithan94@gmail.com>
2026-09-09 12:25:10 +0530

    Add files via upload

---
+++ CO2_6.py
@@ -0,0 +1,11 @@
+s = (input ("enter a string: "))
+freq = {}
+for char in s:
+    if char in freq:
+        freq[char] += 1
+    else:
+        freq[char] = 1
+        
+print ("Char Frequency: ")
+for char, count in freq.items():
+    print (char, ":", count)
